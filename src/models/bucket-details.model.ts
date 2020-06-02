df2427321e8077db89c3bedfd0889fd2fa5b0a78
panachainy <panachai.ny@gmail.com>
2020-06-02 15:31:07 +0700

    add bucket details

---
+++ src/models/bucket-details.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class BucketDetails extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  productId: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  price: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<BucketDetails>) {
+    super(data);
+  }
+}
+
+export interface BucketDetailsRelations {
+  // describe navigational properties here
+}
+
+export type BucketDetailsWithRelations = BucketDetails & BucketDetailsRelations;
--- src/models/index.ts
+++ src/models/index.ts
@@ -7,3 +7,4 @@ export * from './todo.model';
 export * from './test-new-table.model';
 export * from './products.model';
 export * from './buckets.model';
+export * from './bucket-details.model';
